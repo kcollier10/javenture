3cfac5a44211a6bdeebd91c1e8527c25fe064c99
kcollier10 <kristencollier10@gmail.com>
2021-04-24 14:50:01 -0500

    feat: finishing up entities

--- src/main/java/com/postgrad/practice/models/Armor.java
+++ src/main/java/com/postgrad/practice/models/Armor.java
@@ -1,32 +1,69 @@
 package com.postgrad.practice.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "armors")
 public class Armor {
 
-    private final int armorId;
-    private final String name;
-    private final String description;
-    private final int damageBlock;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long armorId;
 
-    public Armor(int armorId, String name, String description, int damageBlock) {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int damageBlock;
+
+    public Armor () {}
+
+    public Armor(long armorId, String name, String description, int damageBlock) {
         this.armorId = armorId;
         this.name = name;
         this.description = description;
         this.damageBlock = damageBlock;
     }
 
-    public int getArmorId() {
+    public Armor(Armor copy) {
+        this.armorId = copy.armorId;
+        this.name = copy.name;
+        this.description = copy.description;
+        this.damageBlock = copy.damageBlock;
+    }
+
+    public long getArmorId() {
         return armorId;
+    }
+
+    public void setArmorId(long armorId) {
+        this.armorId = armorId;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public int getDamageBlock() {
         return damageBlock;
+    }
+
+    public void setDamageBlock(int damageBlock) {
+        this.damageBlock = damageBlock;
     }
 }
--- src/main/java/com/postgrad/practice/models/Portal.java
+++ src/main/java/com/postgrad/practice/models/Portal.java
@@ -17,5 +17,41 @@ public class Portal {
     @JoinColumn(name = "location_id")
     private Location location;
 
+    public Portal() {}
 
+    public Portal(long id, String portalLocation, Location location) {
+        this.id = id;
+        this.portalLocation = portalLocation;
+        this.location = location;
+    }
+
+    public Portal(Portal copy) {
+        this.id = copy.id;
+        this.portalLocation = copy.portalLocation;
+        this.location = copy.location;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPortalLocation() {
+        return portalLocation;
+    }
+
+    public void setPortalLocation(String portalLocation) {
+        this.portalLocation = portalLocation;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
