d56876466522d6c60e8e9ddd0464396c84a17811
kcollier10 <kristencollier10@gmail.com>
2021-04-24 14:56:51 -0500

    finished with starter entities

--- src/main/java/com/postgrad/practice/models/Boss.java
+++ src/main/java/com/postgrad/practice/models/Boss.java
@@ -1,38 +1,69 @@
 package com.postgrad.practice.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bosses")
 public class Boss {
 
-    private final int bossId;
-    private final String name;
-    private final String description;
-    private final int health;
-    private final String voiceline;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bossId;
 
-    public Boss(int bossId, String name, String description, int health, String voiceline) {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int health;
+
+    public Boss() {}
+
+    public Boss(long bossId, String name, String description, int health) {
         this.bossId = bossId;
         this.name = name;
         this.description = description;
         this.health = health;
-        this.voiceline = voiceline;
     }
 
-    public int getBossId() {
+    public Boss(Boss copy) {
+        this.bossId = copy.bossId;
+        this.name = copy.name;
+        this.description = copy.description;
+        this.health = copy.health;
+    }
+
+    public long getBossId() {
         return bossId;
+    }
+
+    public void setBossId(long bossId) {
+        this.bossId = bossId;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public int getHealth() {
         return health;
     }
 
-    public String getVoiceline() {
-        return voiceline;
+    public void setHealth(int health) {
+        this.health = health;
     }
 }
